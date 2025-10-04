e797612613ab6de57acdcc20e04ac29e4351a3e4
vipyan <vipinkaniyanthara@gmail.com>
2025-10-04 18:26:49 -0400

    added route

---
+++ routes/ideas.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Milk cartons that turn a different color the older that your milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+  {
+    id: 3,
+    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
+    tag: 'Software',
+    username: 'BruceBanner',
+    date: '2022-01-02',
+  },
+];
+
+
+//get all ideas
+router.get("/", (req, res) => {
+  res.json({ success: true, data: ideas});
+});
+
+//get single idea
+router.get("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === parseInt(req.params.id));
+    if (!idea) {
+        return res
+            .status(404)
+            .json({ success: false, message: 'Idea not found' });
+    }
+  res.json({ success: true, data: idea});
+});
+
+
+//Add an idea
+router.post("/", (req, res) => {
+  res.send("Add an idea");
+});
+
+
+
+module.exports = router;
